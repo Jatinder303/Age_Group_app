468ad18fa4d7c47f7288b7d8e5dd8e938ae57888
Jatinder303 <Jatinder303@gmail.com>
2023-09-07 19:55:06 +1200

    Add project files.

---
+++ Age_group.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Group_app
+{
+    internal class Age_group
+    {
+        public int age {  get; set; }
+        public string age_group { get; set; }
+
+        public int person_age(DateTime date_of_birth)
+        {
+            DateTime Obj_today = DateTime.Today;
+            age = Obj_today.Year - date_of_birth.Year;
+            return age;
+        }
+
+        public string Person_Age_Group(int age)
+        {
+            
+            if (age >= 65)
+            {
+                age_group = "Senior Citizen";
+            }
+            else if (age >= 30 && age < 65)
+            {
+                age_group = "Middle Adult";
+            }
+            else if (age >= 19 && age < 30)
+            {
+                age_group = "Young Adult";
+            }
+            else if (age >= 13 && age < 19)
+            {
+                age_group = "Adolescents";
+            }
+            else if (age >= 6 && age < 13)
+            {
+                age_group = "Children";
+            }
+            else if (age >= 2 && age < 6)
+            {
+                age_group = "Toddlers";
+            }
+            else if (age >= 0 && age < 2)
+            {
+                age_group = "Infants";
+            }
+            else
+            {
+                age_group = "Invalid";
+            }
+
+            return age_group;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Group_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Age_group obj_age_group = new Age_group();
+            string name, age_group;
+            DateTime dob;
+
+            Console.WriteLine("Please enter the name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter the Date of Birth (yyyy-mm-dd)");
+            dob = Convert.ToDateTime(Console.ReadLine());
+
+            //DateTime Obj_today = DateTime.Today;
+            //int age = Obj_today.Year - dob.Year;
+
+            int age = obj_age_group.person_age(dob);
+            age_group = obj_age_group.Person_Age_Group(age);
+            
+
+            switch(age_group)
+            {
+                case "Senior Citizen":
+                {
+                        Console.WriteLine("Walk is good for you");
+                        break;
+                }
+                case "Middle Adult":
+                    {
+                        Console.WriteLine("2 days a week of activities that strengthen muscles is good for you");
+                        break;
+                    }
+                case "Young Adult":
+                    {
+                        Console.WriteLine("5 days a week of activities that strengthen muscles is good for you");
+                        break;
+                    }
+                case "Adolescents":
+                    {
+                        Console.WriteLine("Play more outdoor games");
+                        break;
+                    }
+                case "Children":
+                    {
+                        Console.WriteLine("Eating less sugar is good for you’");
+                        break;
+                    }
+                case "Toddlers":
+                    {
+                        Console.WriteLine("Don’t go with stranger");
+                        break;
+                    }
+                case "Infants":
+                    {
+                        Console.WriteLine("Enjoy");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid");
+                            break; }
+            }
+           
+            Console.ReadKey();
+        }
+
+
+
+        
+    }
+}
